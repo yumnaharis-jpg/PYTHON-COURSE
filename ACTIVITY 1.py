1323fa8fb18f62fe8c50bf0597bc5ec50124bf7f
yumnaharis-jpg <yumnaharis@gmail.com>
2025-11-27 19:42:31 +0500

    Activity1 codingal

---
+++ ACTIVITY 1.py
@@ -0,0 +1,22 @@
+print ("hello world")
+Dubai = "Burj khalifa"
+print (Dubai)
+France = "Eifil Tower"
+Egypt = "Great Pyramid Giza"
+print("type of Dubai: ", type(Dubai))
+print("type of France: ", type(France))
+print("type of Egypt: ", type(Egypt))
+Burj_Khalifa = 828
+Eiffil_tower = 330
+Pyramid_of_Giza = 137.5
+print("type of Burj_Khalifa: ", type(Burj_Khalifa))
+print("type of Eiffil_tower: ", type(Eiffil_tower))
+print("type of Pyramid_of_Giza: ", type(Pyramid_of_Giza))
+
+
+
+
+
+
+
+
