a6f0e57e814042d30abbb1076e2e6f5d8fef4bb2
yumnaharis-jpg <yumnaharis@gmail.com>
2025-12-11 19:51:06 +0500

    no message :)

--- Conditions.py
+++ Conditions.py
@@ -1,7 +1,18 @@
-num = int(input("enter a number: "))     
-if num >= 0:
-  print(num,"It is a positive number!")
-
+print("enter marks obtained in 5 subjects")
+MARKone = int(input())
+MARKtwo = int(input())
+MARKthree = int(input())
+MARKfour = int(input())
+total = MARKone + MARKtwo + MARKthree + MARKfour
+avg = total/4
+print(avg)
+if avg>=91 and avg <=100:
+  print("Your grade A1")
+elif avg>= 81 and avg <91:
+  print("your grade is A2")
+elif avg>=71 and avg <81:
+  print("your grade is B1")
+  
 
 
 
