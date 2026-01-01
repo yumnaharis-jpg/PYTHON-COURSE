b0a8c699208c5e29a73b8f6bf2776deb8c4a138e
yumnaharis-jpg <yumnaharis@gmail.com>
2026-01-01 19:46:27 +0500

    yesss

---
+++ Invrted_pyramid.py
@@ -0,0 +1,14 @@
+n = int(input("enter a number: "))
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(i):
+        print("*", end="")
+    print()
+
+
+
+
+
+
+
