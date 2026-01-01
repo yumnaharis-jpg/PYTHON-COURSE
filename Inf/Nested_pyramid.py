17cce190ff260589b489f61363974f1fab93a3fa
yumnaharis-jpg <yumnaharis@gmail.com>
2026-01-01 19:19:04 +0500

    look me is pwo : )

---
+++ Inf/Nested_pyramid.py
@@ -0,0 +1,28 @@
+print("Half pyramid pattern of stars (*):")
+n = int(input("enter number of rows"))
+for i in range (n):
+    for j in range (i + 1):
+        print("*", end="")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
