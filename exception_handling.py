1460124b96a0d68c766a6cb9f69916b204babc91
yumnaharis-jpg <yumnaharis@gmail.com>
2026-01-22 19:30:48 +0500

    OP code

---
+++ exception_handling.py
@@ -0,0 +1,16 @@
+try:
+    number = int(input("enter a number:"))
+    print("the number entered is:", number)
+except ValueError as ex:
+    print("Exeption::", ex)
+
+
+
+
+
+
+
+
+
+
+
---
+++ expenses.py
@@ -0,0 +1,14 @@
+rent = int(input("Enter your monthly rent:"))
+electricity_bill = int(input("Enter your monthly electricity bill:"))
+groceries = int(input("Enter your monthly grocery bill:"))
+fuel = int(input("Enter your monthly fuel exepense:"))
+educational_expenses = int(input("Enter your monthly educational expenses:"))
+maid = int(input("Enter your monthly maid salary:"))
+miscellaneous = int(input("enter your monthly miscellaneous expenses:"))
+Total_monthly_expenses = rent + electricity_bill + groceries + fuel + educational_expenses + maid
+print(Total_monthly_expenses)
+print("your total monthly expenses are:", Total_monthly_expenses)
+
+
+
+
