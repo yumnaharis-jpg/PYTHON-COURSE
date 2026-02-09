0eaa1997f5443347e2e8be7f4e3c347f796b27b7
yumnaharis-jpg <yumnaharis@gmail.com>
2026-02-09 19:05:34 +0500

    uhhhhhh coding is boring but it makes you rich XD

---
+++ listing_boring.py
@@ -0,0 +1,28 @@
+empty_list = []
+print()
+numbers = [1,2,3,4,5]
+print(numbers)
+triples = [1,2,3] * 3
+print(triples)
+alist = [100,200,300,400,500]
+print(alist,"\n" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
