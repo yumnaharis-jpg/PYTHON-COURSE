626af10f4fed94221d17ddfa9790efe24a5a16db
yumnaharis-jpg <yumnaharis@gmail.com>
2026-02-26 19:20:49 +0500

    Hope this helps you understand pyth- SIKEEE use this or whatever you want!

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+country_code = {'India' : '0091','Autralia' : '0025',
+'Nepal' : '00977'}
+print("Country code for india -")
+print(country_code.get('India', 'not found'))
+print("country code for japan -")
+print(country_code.get('Japan', 'not found'))
+
+
+
+
+
+
+
+
+
+
+
+
