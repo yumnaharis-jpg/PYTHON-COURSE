a3fa886d08576d3871be0c6f22f3f9c5614b7da0
yumnaharis-jpg <yumnaharis@gmail.com>
2026-02-26 19:42:39 +0500

    W

---
+++ Frequency_the_sequel.py
@@ -0,0 +1,16 @@
+minecraft = {'Minecraft': 2,
+    'is': 2, 'the' : 2, 'best': 1}
+m = 2 
+Count = 0
+for key in minecraft:
+    if minecraft[key] == m:
+        Count = Count + 1
+print("frequency of m is: " + str(Count))
+
+
+
+
+
+
+
+
