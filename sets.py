b6c7872d7785af7d07d3fa2a8d51f50e1db37cae
yumnaharis-jpg <yumnaharis@gmail.com>
2026-03-05 19:31:19 +0500

    mathhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ sets.py
@@ -0,0 +1,24 @@
+my_set = {1,2,3}
+print(my_set)
+my_set2 = {1.0,"hello",(1,2,3)}
+print(my_set2)
+my_set3 = {1,2,3,4,3,2}
+print(my_set3)
+my_set4 = set([1,2,3,2])
+print(my_set4, "\n")
+num_set = set([0,1,2,3,4,5])
+print("original set:")
+print(num_set)
+num_set.pop()
+print("after removing the first element from the said set:")
+print(num_set,"\n")
+
+
+
+
+
+
+
+
+
+
