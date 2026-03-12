f1f20c9613861d8c84b3b02779115838a160622e
yumnaharis-jpg <yumnaharis@gmail.com>
2026-03-12 19:44:20 +0500

    STOCKS ARE RISING!

---
+++ zip.py
@@ -0,0 +1,20 @@
+s1 = [1, 2, 3]
+s2 = [4, 5, 6]
+zipped = zip(s1, s2)
+print(list(zipped))
+
+stocks = ["AAPL", "GOOGL", "MSFT"]
+prices = [150, 2800, 300]
+stock_data = list(zip(stocks, prices))
+print(stock_data)
+
+
+
+
+
+
+
+
+
+
+
