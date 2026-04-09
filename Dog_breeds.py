8dcfd449eb94aa3c48eaad892076d113b305d44c
yumnaharis-jpg <yumnaharis@gmail.com>
2026-04-09 19:38:50 +0500

    CLLASEESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

---
+++ Dog_breeds.py
@@ -0,0 +1,22 @@
+class dog:
+    def __init__(self, breed, color):
+        self.breed = breed
+        self.color = color
+dog1 = dog("Labrador", "Yellow")
+print("Dog Breed:", dog1.breed)
+print("Dog Color:", dog1.color)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class_veichle.py
@@ -0,0 +1,15 @@
+class vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+modelX = vehicle(240, 18)
+print("Model Max Speed:",modelX.max_speed)        
+print("Model Mileage:",modelX.mileage)
+
+
+
+
+
+
+
+
